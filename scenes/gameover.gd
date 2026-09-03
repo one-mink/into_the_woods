f728817d5fb3ea64370261d28783d47932715b30
archaeopteriks <dsoe.leon@gmail.com>
2026-09-03 15:40:58 +0200

    fixed bugs

--- scenes/gameover.gd
+++ scenes/gameover.gd
@@ -2,4 +2,5 @@ extends Node2D
 
 
 func _on_button_pressed() -> void:
+	Fuel.fuel = 0
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
--- scripts/tarder.gd
+++ scripts/tarder.gd
@@ -8,8 +8,8 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if Mushroom.count > 0:
-		Mushroom.count -= 1
-		Fuel.fuel += 10
+		Fuel.fuel += 10*Mushroom.count
+		Mushroom.count -= Mushroom.count	
 		print(Fuel.fuel)
 		
 	
