d02dd5c1b19ffd4e5a33dbce99d96ba4b32aeff2
one-mink <dsoe.leon@gmail.com>
2026-09-07 17:15:08 +0200

    fixed number

--- scripts/gameover.gd
+++ scripts/gameover.gd
@@ -4,7 +4,7 @@ extends Node2D
 func _on_button_pressed() -> void:
 	
 	Fuel.fuel = 0
-	Player.hearts = 0
+	Player.hearts = 5
 	Mushroom.count = 0
 	
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
